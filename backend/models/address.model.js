1dccaaeaa9d1a4385e257bd30319db7d3308d67d
Shirley Sosa <slaymesosa@gmail.com>
2020-09-18 13:58:29 -0400

    first push

---
+++ backend/models/address.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const addressSchema = new Schema({
+    street: {type: String},
+    city:{type: String},
+    state:{type: String},
+    zipcode: {type: String}
+});
+
+const Address = mongoose.model('Address', addressSchema);
+
+module.exports = Address;
---
+++ backend/models/user.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    
+    profilepic: {type: String},
+    firstname: {type: String, required: true},
+    lastname: {type: String, required: true},
+    phonenumber: {type: String, required: true},
+    email: {type: String, required: true}    
+
+});
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
