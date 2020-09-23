fc6fbd5d647bedf07be2817d559e9b1d60e85978
Matias Tong <matias.tong@gmail.com>
2020-09-23 12:32:45 -0400

    running, fixed issues

--- client/novi/src/App.js
+++ client/novi/src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./components/navbar.component";
+// import Navbar from "./components/navbar.component";
 import CreateUser from "./components/create-user.component";
 
 function App() {
     return (
-        <Router>
             <div className="container">
-                <Navbar />
+                {/* <Navbar /> */}
                 <br />
                 {/* Route paths go here */}
                 <Route path="/user" component={CreateUser} />
             </div>
-        </Router>
     );
 }
 
