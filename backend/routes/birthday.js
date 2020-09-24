a33ee7367a340152f64bb98cf744f2ca3af8325d
JavaJo <jpl212011@gmail.com>
2020-09-23 22:03:02 -0400

    updated route for address and birthday

--- backend/routes/birthday.js
+++ backend/routes/birthday.js
@@ -3,7 +3,7 @@ const birthdayModel = require('../models/birthday.model.js');
 const router = express.Router();
 
 
-// Create
+// Create birthdays
 router.post('/birthday', async (req, res) => {
     const birthday = new birthdayModel(req.body);
     try {
@@ -14,7 +14,7 @@ router.post('/birthday', async (req, res) => {
     }
 });
 
-//Read 
+//Read birthdays
 router.get('/birthday', async (req, res) => {
     const birthday = await birthdayModel.find({});
     try {
@@ -24,7 +24,17 @@ router.get('/birthday', async (req, res) => {
     }
 });
 
-//Update
+//Read by ID
+router.get("/birthday/:id", async (req, res) => {
+	try {
+		const reminder = await birthdayModel.findById(req.params.id)
+		res.send(birthday)
+	} catch (err){
+		res.status(404).send("That birthday doesn't exist!")
+	}
+});
+
+//Update birthdays
 router.patch('/birthday/:id', async (req, res) => {
     try {
         await birthdayModel.findByIdAndUpdate(req.params.id, req.body)
@@ -35,7 +45,7 @@ router.patch('/birthday/:id', async (req, res) => {
     }
 });
 
-// Delete
+// Delete birthdays
 router.delete('/birthday/:id', async (req, res) => {
     try {
         const birthday = await birthdayModel.findByIdAndDelete(req.params.id)
