4f7e3835fc9ab04d108b3a5bc9cd829ece1d51cb
Shirley Sosa <slaymesosa@gmail.com>
2020-09-25 11:53:53 -0400

    contactform

--- client/novi/src/pages/AddContact.jsx
+++ client/novi/src/pages/AddContact.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import Birthday from "../components/Birthday"
 import Holiday from "../components/Holiday"
 import Container from "react-bootstrap/Container"
-import ContactForm from "../components/create-contactform.component"
+import ContactForm from "../components/AddContactCard"
 
 function AddContact() {
     return (
